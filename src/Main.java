431d0be5205988b975a08b6f6f9cc360ede558cb
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2022-02-04 18:10:09 -0500

    Everything up to #7

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,62 @@ public class Main
 {
     public static void main(String[] args)
     {
+        loadStudents();
         // TODO: make an ArrayList of type Double (for the constants)
+        List <Double> constants  = new ArrayList<>(loadConstants());
         // TODO: make a LinkedList of type Student (for the students)
+        List <Student> sStudent  = new LinkedList<>(loadStudents());
 
         // TODO: call a method to load constants, returning an ArrayList of Doubles
+        loadConstants();
         // TODO: call a method to load students, returning a LinkedList of Students
+        loadStudents();
         
         // TODO: call the printStuff method with the list of constants
+        printStuff(constants);
         // TODO: call the printStuff method with the list of students
-        
+        printStuff(sStudent);
+
         // TODO: sort the list of students with Collections.sort()
+        Collections.sort(sStudent);
         // TODO: call the printStuff method again with the list of students
+        printStuff(sStudent);
         
         // TODO: call the trimStudents() method
         // TODO: call the printStuff method again with the list of students
     }
     
     // TODO: create a method loadConstants() which returns an ArrayList of Doubles
+    private static ArrayList<Double> loadConstants()
+    {
+        File file = new File("res/constants.txt");
+        Scanner reader;
+
+        try
+        {
+            reader = new Scanner(file);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println("Could not find file: " + file.toPath());
+            return null;
+        }
+
+        ArrayList<Double> constants = new ArrayList<Double>();
+
+        reader.nextLine(); // eat header
+        while (reader.hasNext())
+        {
+            String line = reader.nextLine();
+            Double d = Double.valueOf(line);
+            constants.add(d);
+        }
+
+        return constants;
+    }
 
     // TODO: uncomment this method below
-    /*  
+
     private static LinkedList<Student> loadStudents()
     {
         File file = new File("res/roster.csv");
@@ -61,12 +97,22 @@ public class Main
 
         return students;
     }
-    */
-    
+
     // TODO: create a printStuff() method that takes a List of anything and prints it
-    
+
+    public static <E> void printStuff(List <E> stuff)
+    {
+        for (E element : stuff)
+            System.out.println(element);
+    }
+
     // TODO: create a trimStudents() method that takes a LinkedList of Students
     // and a double for the filter amount, which then iterates through the
     // LinkedList of Students with an Iterator and removes any Student with
     // a grade less than 90
+
+    private static LinkedList<Student> trimStudents(double grade)
+    {
+
+    }
 }
---
+++ src/Student.java
@@ -0,0 +1,42 @@
+public class Student implements Comparable <Student>
+
+{
+    private String LName;
+    private String FName;
+    private float SGrade;
+
+    public Student(String last_name, String first_name, float grade)
+    {
+        LName = last_name;
+        FName = first_name;
+        SGrade = grade;
+    }
+
+    public String getLastName()
+    {
+        return LName;
+    }
+
+    public String getFirstName()
+    {
+        return FName;
+    }
+
+    public float getGrade()
+    {
+        return SGrade;
+    }
+
+    public String toString()
+    {
+        return String.format("[%-20s] [%-20s] - [%5.2f", LName, FName, SGrade);
+    }
+
+    @Override
+    public int compareTo(Student o)
+    {
+        return this.getLastName().compareTo(o.getLastName());
+    }
+}
+
+
