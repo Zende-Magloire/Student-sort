fd2bf79d2b70e62714221de4de156f6b25daf759
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2022-02-06 21:10:05 -0500

    something

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        loadStudents();
         // TODO: make an ArrayList of type Double (for the constants)
         List <Double> constants  = new ArrayList<>(loadConstants());
         // TODO: make a LinkedList of type Student (for the students)
@@ -20,19 +19,26 @@ public class Main
         loadStudents();
         
         // TODO: call the printStuff method with the list of constants
+        System.out.println("Constants:");
         printStuff(constants);
         // TODO: call the printStuff method with the list of students
+        System.out.println("Students (unsorted):");
         printStuff(sStudent);
 
         // TODO: sort the list of students with Collections.sort()
         Collections.sort(sStudent);
         // TODO: call the printStuff method again with the list of students
+        System.out.println("Students (sorted):");
         printStuff(sStudent);
         
         // TODO: call the trimStudents() method
+        trimStudents(sStudent, 90);
         // TODO: call the printStuff method again with the list of students
+        System.out.println("Students (A's)");
+        printStuff(sStudent);
     }
-    
+
+
     // TODO: create a method loadConstants() which returns an ArrayList of Doubles
     private static ArrayList<Double> loadConstants()
     {
@@ -49,7 +55,7 @@ public class Main
             return null;
         }
 
-        ArrayList<Double> constants = new ArrayList<Double>();
+        ArrayList<Double> constants = new ArrayList<>();
 
         reader.nextLine(); // eat header
         while (reader.hasNext())
@@ -79,7 +85,7 @@ public class Main
             return null;
         }
 
-        LinkedList<Student> students = new LinkedList<Student>();
+        LinkedList<Student> sStudents = new LinkedList<>();
 
         reader.nextLine(); // eat header
         while (reader.hasNext())
@@ -92,10 +98,10 @@ public class Main
             float grade = Float.valueOf(tokenizer.nextToken());
 
             Student s = new Student(first, last, grade);
-            students.add(s);
+            sStudents.add(s);
         }
 
-        return students;
+        return sStudents;
     }
 
     // TODO: create a printStuff() method that takes a List of anything and prints it
@@ -111,8 +117,14 @@ public class Main
     // LinkedList of Students with an Iterator and removes any Student with
     // a grade less than 90
 
-    private static LinkedList<Student> trimStudents(double grade)
+    private static void trimStudents(List<Student> sStudent, float grade)
     {
-
+        Iterator<Student> itr = sStudent.iterator();
+        for (Student st : sStudent) {
+            float sGrade = st.getGrade();
+            if (sGrade < grade)
+                itr.remove();
+        }
+        System.out.println(sStudent);
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+
+
 public class Student implements Comparable <Student>
 
 {
